7bba139d6f4d84c7c51298d0aec7a8e63f4640ad
AntsmithCode <hey@antsmith.dev>
2021-01-08 16:58:30 -0500

    (APP) Finalize and tested conversion formulas II

--- root/app.js
+++ root/app.js
@@ -4,18 +4,56 @@ function tempConvert() {
 
   switch (scaleCalc) {
     case "Fahrenheit": //Takes the value of fahrenheit and converts to other scales
-      document.querySelector("#getCelsius").innerHTML =
-        Math.round(inputNum * (9 / 5) + 32) + " C";
+      document.querySelector("#getFahrenheit").innerHTML = inputNum + " F";
 
-      document.querySelector("#getFahrenheit").innerHTML =
-        Math.round(inputNum) + " F";
+      document.querySelector("#getCelsius").innerHTML =
+        (inputNum - 32) * (5 / 9) + " C";
 
       document.querySelector("#getKelvin").innerHTML =
         (inputNum - 32) * (5 / 9) + 273.15 + " K";
 
       document.querySelector("#getRankine").innerHTML =
         inputNum + 459.67 + " R";
-
       break;
+
+    case "Celsius":
+      document.querySelector("#getCelsius").innerHTML = inputNum + " C";
+
+      document.querySelector("#getFahrenheit").innerHTML =
+        inputNum * (9 / 5) + 32 + " F";
+
+      document.querySelector("#getKelvin").innerHTML = inputNum + 273.15 + " K";
+
+      document.querySelector("#getRankine").innerHTML =
+        inputNum + 9 / 5 + 491.67 + " R";
+      break;
+
+    case "Kelvin":
+      document.querySelector("#getCelsius").innerHTML =
+        inputNum - 273.15 + " C";
+
+      document.querySelector("#getFahrenheit").innerHTML =
+        (inputNum - 273.15) * (9 / 5) + 32 + " F";
+
+      document.querySelector("#getKelvin").innerHTML = inputNum + " K";
+
+      document.querySelector("#getRankine").innerHTML = inputNum * 1.8 + " R";
+      break;
+
+    case "Rankine":
+      document.querySelector("#getCelsius").innerHTML =
+        ((inputNum - 491.67) * 5) / 9 + " C";
+
+      document.querySelector("#getFahrenheit").innerHTML =
+        inputNum - 459.67 + " F";
+
+      document.querySelector("#getKelvin").innerHTML =
+        inputNum * (5 / 9) + " K";
+
+      document.querySelector("#getRankine").innerHTML = inputNum + " R";
+      break;
+
+    default:
+      console.log("Select a scale and enter a value");
   }
 }
